e883cda510b88276190756334951fd1bc10d987a
vishwas <vishwas452003@gmail.com>
2024-08-27 01:12:27 +0700

    add sound effect,courage hand

--- src/App.jsx
+++ src/App.jsx
@@ -16,16 +16,30 @@ function App() {
      audio_theme.currentTime = 0; // Reset the audio to the beginning
    };
 
-   //for keyboard noises
-   function keyboard_press() {
-     
-   }
+  
  }, []);
 
 
   return (
     <>
       <div className="main-container">
+        <div className="icon-sound">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="1em"
+            height="1em"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="1.5"
+              d="M1.75 5.75v4.5h2.5l4 3V2.75l-4 3zm9 .5s1 .5 1 1.75s-1 1.75-1 1.75m1-6.5c2 1 3 2.5 3 4.75s-1 3.75-3 4.75"
+            />
+          </svg>
+        </div>
         <div className="response-container">
           <div className="response-left">
             <img
@@ -33,7 +47,7 @@ function App() {
               alt=""
               className="dog-image"
             />
-            <h3 className="courage-text">COURAGE-GPT</h3>
+            <h3 className="courage-text">COURAGE-GPR</h3>
           </div>
           <div className="response-right">
             <div className="help-box">
