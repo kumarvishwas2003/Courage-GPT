3eb3c4fafd7ade70613f44e5730bb0332eac98b0
vishwas <vishwas452003@gmail.com>
2024-09-18 16:11:57 +0530

    added villians

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,9 +5,25 @@ import { Sound_btn } from "./components/Sound_btn/Sound_btn";
 import courage_house from "/src/assets/courage-house.jpg";
 import courage_dog from "/src/assets/courage-dog.gif";
 import courage_computer from "/src/assets/computer.png";
+import footImage from "/src/assets/villians/foot.webp";
+import fredImage from "/src/assets/villians/fred.webp";
+import katzeImage from "/src/assets/villians/katze.webp";
+import catImage from "/src/assets/villians/cat.webp";
+import duckImage from "/src/assets/villians/duck.webp";
+import ramsImage from "/src/assets/villians/rams.webp";
+
 
 const Home = () => {
   // let root = `./src/assets/villians/`;
+  const villainImages = {
+    foot: footImage,
+    fred: fredImage,
+    katze: katzeImage,
+    cat: catImage,
+    duck: duckImage,
+    rams: ramsImage,
+  };
+
   const villians = ["foot", "fred", "katze", "cat", "duck", "rams"];
 
   const [currentVillian, nextVillian] = useState(0);
@@ -46,7 +62,7 @@ const Home = () => {
               className={`${villian}-div villian`}
               style={{ display: currentVillian === index ? "flex" : "none" }}
             >
-              <img src={`./src/assets/villians/${villian}.webp`} alt="" />
+              <img src={villainImages[villian]} alt={villian} />
             </div>
           ))}
         </div>
